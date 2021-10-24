64d2c778a0be3fe7ed9cd4494d88a223c8638b6b
sergio2000 <42353861+sergio2000@users.noreply.github.com>
2021-10-24 13:52:52 -0700

    Add files via upload

---
+++ Clima/Clima/Model/WeatherModel.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by sergioacosta on 6/10/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    var aProperty: Int {
+        return 2 + 5
+    }
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "snow"
+        case 701...781:
+            return "sun.haze"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.sun"
+        default:
+            return "cloud"
+        }
+    }
+}
